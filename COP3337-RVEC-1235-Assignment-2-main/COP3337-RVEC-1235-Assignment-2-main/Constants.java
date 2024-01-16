d44725521d7660b3f8515dfd7a23dd059da84c77
Doc <69441729+doc88129@users.noreply.github.com>
2024-01-15 21:17:23 -0500

    Add files via upload

---
+++ COP3337-RVEC-1235-Assignment-2-main/COP3337-RVEC-1235-Assignment-2-main/Constants.java
@@ -0,0 +1,5 @@
+public interface Constants {
+    static final int WRAP_AROUND = 26;
+    static final int ENCODE_SHIFT = 3;
+    static final int DECODE_SHIFT = 23;
+}
---
+++ COP3337-RVEC-1235-Assignment-2-main/COP3337-RVEC-1235-Assignment-2-main/Reverser.java
@@ -0,0 +1,12 @@
+public class Reverser extends Transpose {
+    public Reverser(String s) {
+        
+    }
+    public String reverseText(String word) { 
+        
+    }
+
+    public String decode(String word) {
+        
+    }
+}
